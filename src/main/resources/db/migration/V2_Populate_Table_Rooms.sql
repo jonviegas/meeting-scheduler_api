cef586be0936d8a22649cb0c19382edaa9efc682
jonviegas <jonnyalmeida95@gmail.com>
2022-07-15 18:35:03 -0300

    populate query removed

--- src/main/resources/db/migration/V2_Populate_Table_Rooms.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `rooms` (`name`, `date`, `start_hour`, `end_hour`) VALUES
-	('Docker Basics', CURRENT_DATE, '02:00:00', '04:00:00'),
-	('Java OOP', CURRENT_DATE, '03:00:00', '05:00:00');
